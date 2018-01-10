47d398ec1b8f05f330a8cf3b9a2065689c85ff1b
Lucas Stark <lucas.stark@live.com>
2018-01-10 07:28:18 -0500

    1.0.8 - Trigger check variations so pricing tables load initially.

--- assets/js/front-end.js
+++ assets/js/front-end.js
@@ -6,14 +6,20 @@
         var $form = $(e.target);
 
         if ($form.data('has_dynamic_pricing_table') !== 1) {
-            $form.data('has_dynamic_pricing_table', 1)
-            $form.on('show_variation', function (e, variation, purchasable) {
+            $form.data('has_dynamic_pricing_table', 1);
+
+
+            $form.on('show_variation', function (e, variation) {
 
                 $form.find('table.dynamic-pricing-table').hide();
                 $form.find('table.dynamic-pricing-table-variation-' + variation.variation_id).show();
 
 
             });
+
+            setTimeout( function() {
+                $form.trigger( 'check_variations' );
+            }, 50 );
         }
 
 
